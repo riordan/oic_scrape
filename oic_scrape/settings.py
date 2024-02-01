0c473bbbf4624258ea02d05ef1cbb1d97246aae0
David Riordan <dr@daveriordan.com>
2024-02-01 15:26:24 -0500

    add scrapy-playwright and playwright based settings

--- oic_scrape/settings.py
+++ oic_scrape/settings.py
@@ -91,3 +91,8 @@ HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
