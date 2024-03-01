b5c17810a7373a5a70d1427729ca4da1249bcca6
David Riordan <dr@daveriordan.com>
2024-02-29 22:55:53 -0500

    Adds new AwardItem to IMLS

--- oic_scrape/spiders/imls_gov.py
+++ oic_scrape/spiders/imls_gov.py
@@ -1,16 +1,17 @@
 import scrapy
-from oic_scrape.items import GrantItem
-from datetime import datetime
+from oic_scrape.items import AwardItem
+from datetime import datetime, date
+from attrs import asdict
 
 FUNDER_NAME = "Institute of Museum and Library Services"
 FUNDER_ROR = "https://ror.org/030prv062"
+
 
 class ImlsGovSpider(scrapy.Spider):
     name = "imls.gov_grants"
 
     allowed_domains = ["imls.gov"]
-    start_urls = ['https://www.imls.gov/grants/awarded-grants']
-
+    start_urls = ["https://www.imls.gov/grants/awarded-grants"]
 
     def parse(self, response):
         """
@@ -21,14 +22,14 @@ class ImlsGovSpider(scrapy.Spider):
 
         """
         # Extracting the links to the grant detail pages
-        for grant_link in response.css('td.views-field-title a::attr(href)').getall():
+        for grant_link in response.css("td.views-field-title a::attr(href)").getall():
             yield response.follow(grant_link, self.parse_grant)
 
         # Following the pagination link
-        next_page = response.css('.pager__item--next a::attr(href)').get()
+        next_page = response.css(".pager__item--next a::attr(href)").get()
         if next_page is not None:
             yield response.follow(next_page, self.parse)
-    
+
     def parse_grant(self, response):
         """
         @url https://imls.gov/grants/awarded/mh-253516-oms-23
@@ -37,52 +38,69 @@ class ImlsGovSpider(scrapy.Spider):
         @scrapes grant_id program_of_funder grant_year award_amount award_currency award_amount_usd recipient_org_name recipient_location grant_description funder_name funder_ror_id source _crawled_at
         """
 
-        # Extract values
+        # Extract all displayed data from the page
+        institution = response.css(".field--name-field-institution::text").get()
+        log_number = response.css(".title--small > span::text").get()
+        program = response.css(
+            "div.field--name-field-program-categories-text .field__item::text"
+        ).get()
+        fiscal_year = response.css(
+            "div.field--name-field-fiscal-year-text .field__item::text"
+        ).get()
+        federal_funds = response.css(
+            'div.field .field__label:contains("Federal Funds") + .field__item::text'
+        ).get()
+        city = response.css("div.field--name-field-city .field__item::text").get()
+        state = response.css("div.field--name-field-states .field__item::text").get()
+        body = response.css("div.clearfix:nth-child(4)::text").get()
+        # store source data
+        source_data = {
+            "url": response.url,
+            "institution": institution,
+            "log_number": log_number,
+            "program": program,
+            "fiscal_year": fiscal_year,
+            "federal_funds": federal_funds,
+            "city": city,
+            "state": state,
+            "body": body,
+        }
 
-        imls_log_number_raw = response.css('.title--small > span::text').get()
-        if imls_log_number_raw:
-            imls_log_number = imls_log_number_raw.strip()
-        else:
-            imls_log_number = None  # Or some default value
-
-
-        id = f"imls:log_number::{imls_log_number}"
-        program_of_funder = response.css('div.field--name-field-program-categories-text .field__item::text').get()
-        grant_year = response.css('div.field--name-field-fiscal-year-text .field__item::text').get()
-
-        award_amount_raw = response.css('div.field .field__label:contains("Federal Funds") + .field__item::text').get()
-        if award_amount_raw:
-                award_amount = award_amount_raw.strip()
-        else:
-            award_amount = None
-        
-        award_currency = 'USD'  # Assuming USD for simplicity
-        award_amount_usd = award_amount  # Assuming amount is already in USD
-        city = response.css('div.field--name-field-city .field__item::text').get()
-        state = response.css('div.field--name-field-states .field__item::text').get()
-        recipient_org_name = response.css('.field--name-field-institution::text').get()
+        # Formatting fields
+        id = f"imls:log_number::{log_number}"
+        award_amount_formatted = float(federal_funds.replace("$", "").replace(",", ""))
+        award_currency = "USD"  # Assuming USD for simplicity
+        award_amount_usd = award_amount_formatted  # Assuming amount is already in USD
         recipient_location = f"{city}, {state}"
-        funder_name = FUNDER_NAME 
-        funder_ror_id = FUNDER_ROR
-        grant_description = response.css('div.clearfix:nth-child(4)::text').get() #least bad way of doing this for now?
-        _crawled_at = datetime.utcnow()
+
+        # calculate fiscal year start/end date
+        fy = int(fiscal_year)
+        fy_start_date = date(fy - 1, 10, 1)
+        fy_end_date = date(fy, 9, 30)
+
+        comment = f"Award issued for US Federal Fiscal Year {fy}, starting on {fy_start_date} and ending on {fy_end_date}. Specific calendar year of issuance could not be determined from available data."
 
         # Create an instance of GrantItem
-        item = GrantItem(
-            grant_id = id,
-            program_of_funder=program_of_funder,
-            grant_year=grant_year,
-            award_amount=award_amount,
+        item = AwardItem(
+            _crawled_at=datetime.utcnow(),
+            source="imls.gov",
+            grant_id=id,
+            funder_org_name=FUNDER_NAME,
+            funder_org_ror_id=FUNDER_ROR,
+            recipient_org_name=institution,
+            recipient_org_location=recipient_location,
+            grant_start_date=fy_start_date,
+            grant_end_date=fy_end_date,
+            award_amount=award_amount_formatted,
             award_currency=award_currency,
             award_amount_usd=award_amount_usd,
-            recipient_org_name=recipient_org_name,
-            recipient_location=recipient_location,
-            grant_description=grant_description,
-            funder_name=funder_name,
-            funder_ror_id=funder_ror_id,
-            source = 'imls.gov',
-            _crawled_at=_crawled_at
+            source_url=response.url,
+            grant_description=body,
+            program_of_funder=program,
+            comments=comment,
+            raw_source_data=str(source_data),
+            _award_schema_version="0.1.0",
         )
 
         # Return the populated item
-        yield item
+        yield asdict(item)
